152e51906d1cb6c0557444f6d2ed051404a17e75
Kulvir Kaur <balkulvir@gmail.com>
2024-12-31 03:15:19 -0800

    Home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button"
+import FeaturedProducts from "@/components/home/FeaturedProducts"
+import HeroContainer from "@/components/home/HeroContainer"
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">HomePage</h1>
-      <Button variant='outline' size='lg' className="capitalize m-8"> 
-        Click Me 
-      </Button>
-    </div>
+    <>
+      <HeroContainer />
+      <FeaturedProducts />
+    </>
   )
 }
 
